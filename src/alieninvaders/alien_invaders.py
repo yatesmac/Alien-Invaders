a9842aa8184840b93b5decaaebd31f756dbd96ea
Mac_Y <yatsie@live.com>
2021-04-12 20:38:40 +0200

    Added bullets module and associated functionality. Resized the ship.

--- src/alieninvaders/alien_invaders.py
+++ src/alieninvaders/alien_invaders.py
@@ -6,6 +6,7 @@ import pygame as pg
 from pygame.locals import (
     K_LEFT,
     K_RIGHT,
+    K_SPACE,
     K_ESCAPE,
     KEYDOWN,
     KEYUP,
@@ -14,6 +15,7 @@ from pygame.locals import (
 
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
 
 
 class AlienInvasion:
@@ -22,16 +24,20 @@ class AlienInvasion:
     def __init__(self):
         """Initialize the game, and create game resources."""
         self._init_pygame()
+        self.settings = Settings()
         self.screen = pg.display.set_mode(
-            (Settings.SCREEN_WIDTH, Settings.SCREEN_WIDTH)
+            (self.settings.screen_width, self.settings.screen_height)
         )
+
         self.ship = Ship(self)
+        self.bullets = pg.sprite.Group()
 
     def run_game(self):
         """Start main loop for the game."""
         while True:
             self._check_events()
             self.ship.update()
+            self._update_bullets()
             self._update_screen()
 
     @staticmethod
@@ -42,6 +48,7 @@ class AlienInvasion:
     def _check_events(self):
         """Respond to key presses and mouse events."""
         for event in pg.event.get():
+            # Quit
             if event.type == QUIT or (
                 event.type == KEYDOWN and
                 event.key == K_ESCAPE
@@ -49,23 +56,50 @@ class AlienInvasion:
                 sys.exit()
 
             elif event.type == KEYDOWN:
-                if event.type == K_RIGHT:
-                    self.ship.moving_right = True
-                elif event.key == K_LEFT:
-                    self.ship.moving_left = True
-
+                self._check_keydown_events(event)
             elif event.type == KEYUP:
-                if event.key == K_RIGHT:
-                    self.ship.moving_right = False
-                elif event.key == K_LEFT:
-                    self.ship.moving_left = False
+                self._check_keyup_events(event)
+
+    def _check_keydown_events(self, event):
+        """Respond to key presses."""
+        if event.key == K_RIGHT:
+            self.ship.moving_right = True
+        elif event.key == K_LEFT:
+            self.ship.moving_left = True
+        elif event.key == K_SPACE:
+            self._fire_bullet()
+
+    def _check_keyup_events(self, event):
+        """Respond to key releases."""
+        if event.key == K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == K_LEFT:
+            self.ship.moving_left = False
+
+    def _fire_bullet(self):
+        """Create a new bullet and add it to the bullets group."""
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
+
+    def _update_bullets(self):
+        """Update position of bullets and get rid of old bullets."""
+        # Update bullet positions
+        self.bullets.update()
+
+        # Get rid of bullets that have disappeared
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
 
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
-            self.screen.fill(Settings.BG_COLOR)
-            self.ship.blitme()
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
+        pg.display.flip()
 
-            pg.display.flip()
 
 if __name__ == '__main__':
     # Make a game instance, and run the game.
--- src/alieninvaders/settings.py
+++ src/alieninvaders/settings.py
@@ -1,8 +1,22 @@
 class Settings:
-    """A class to store all settings for Alient Invaders"""
+    """A class to store all settings for Alien Invaders"""
 
-    SCREEN_WIDTH = 1200
-    SCREEN_HEIGHT = 800
-    BG_COLOR = (230, 230, 230)
+    # Constant game settings
+    # Screen settings
+    screen_width = 1200
+    screen_height = 800
+    bg_color = (230, 230, 230)
+
+    # Bullet settings
+    bullet_speed = 1.0
+    bullet_width = 3
+    bullet_height = 70
+    bullet_color = (0, 255, 55)
+    bullets_allowed = 5
+
+    def __init__(self):
+        """Initialize the game's settings."""
+        # Ship settings
+        self.ship_speed = 1.5
 
 
